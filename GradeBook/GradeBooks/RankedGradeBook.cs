c4ed041877bd38a8bf14f989bd4027e5bd15b539
F_Kowal <46751405+F-Kowal@users.noreply.github.com>
2020-10-16 11:06:54 +0200

    Add GetLetterGrade method to RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -14,7 +15,39 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("You need 5 or more students");
+            }
+
+            int gradesPercentage = (int)(Math.Round(Students.Count * 0.2));
+
+            List<double> allGrades = Students
+                .OrderByDescending(student => student.AverageGrade)
+                .Select(student => student.AverageGrade)
+                .ToList();
+
+            if(allGrades[gradesPercentage - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (allGrades[(gradesPercentage * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (allGrades[(gradesPercentage * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (allGrades[(gradesPercentage * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
         }
     }
 }
