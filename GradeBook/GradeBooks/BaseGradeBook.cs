b6e3a00c59076d2ba614ce0dc103dc0104a948ad
F_Kowal <46751405+F-Kowal@users.noreply.github.com>
2020-10-16 11:58:42 +0200

    Add IsWeighted property to BaseGradeBook

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -12,6 +12,7 @@ namespace GradeBook.GradeBooks
     public abstract class BaseGradeBook
     {
         public string Name { get; set; }
+        bool isWeighted { get; set; }
         public GradeBookType Type { get; set; }
         public List<Student> Students { get; set; }
 
